01b1b0e8961895bd150ecde440993d08026a6899
Gokul <pgokulmca@gmail.com>
2018-12-30 01:16:31 +0530

    functionalities are componentized with new app router

---
+++ src/components/AddExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AddExpensePage = () => (
+    <div>
+        <p> addExpensePage </p>
+    </div>
+);
+
+export default AddExpensePage;
---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EditExpensePage = () => (
+    <div>
+        <p> editExpensePage </p>
+    </div>
+);
+
+export default EditExpensePage;
