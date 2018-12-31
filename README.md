8a1e2799da7ffbbc9cc9fee628bf946041af2c89
Gokul <pgokulmca@gmail.com>
2018-12-31 22:17:36 +0530

    redux js added to the application

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 #### Add react router dom
     yarn add react-router-dom@4.3.1
     
-####     
+#### Adding redux
+    yarn add redux@4.0.1
+           
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,6 @@ const Header = () => (
         <h1>Expensify App</h1>
         <NavLink to='/' activeClassName='is-active' exact={true}> Home </NavLink>
         <NavLink to='/create' activeClassName='is-active'> Add expense </NavLink>
-        <NavLink to='/edit' activeClassName='is-active'> Edit expense </NavLink>
         <NavLink to='/help' activeClassName='is-active'> Help </NavLink>
     </div>
 );
