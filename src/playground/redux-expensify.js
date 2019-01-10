0c19471c9f30b279849e21442c471f68f3870779
Gokul <pgokulmca@gmail.com>
2019-01-11 00:04:44 +0530

    set text filter added

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -80,11 +80,22 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
     switch (action.type) {
+        case 'SET_TEXT_FILTER':
+            return {
+                ...state,
+                text: action.text
+            }
         default:
             return state;
 
     }
 };
+
+const setTextFilter = (text = '') => ({
+    type: 'SET_TEXT_FILTER',
+    text
+
+})
 
 
 // store creation
@@ -100,8 +111,8 @@ const store = createStore(
 
 console.log(store.getState());
 
-const exp1 = store.dispatch(addExpense({ description: 'test', note: 'test', amount: 3000}));
-const exp2 = store.dispatch(addExpense({ description: 'test1', note: 'test1', amount: 5000}));
+const exp1 = store.dispatch(addExpense({ description: 'Rent', note: 'Rent', amount: 3000}));
+const exp2 = store.dispatch(addExpense({ description: 'Coffee', note: 'Coffee', amount: 5000}));
 console.log('exp2', exp2.expense.id);
 
 console.log(store.getState());
@@ -109,6 +120,9 @@ console.log(store.getState());
 store.dispatch(removeExpense({ id: exp1.expense.id }));
 
 store.dispatch(editExpense(exp2.expense.id, { amount: 4000 }));
+
+store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter());
 
 console.log(store.getState());
 
